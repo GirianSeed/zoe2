69a16fb7c5c0c546cd6e6597d9f8ed7ee72f1459
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-12-06 13:59:09 -0500

    sound: add iop_main skeleton

---
+++ module/sound/sd_incl.h
@@ -0,0 +1,17 @@
+#ifndef __KCEJ_SD_INCL_H__
+#define __KCEJ_SD_INCL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+//
+//
+//
+//
+
+#ifdef __cplusplus
+}
+#endif
+#endif // {{{ END OF FILE }}}
