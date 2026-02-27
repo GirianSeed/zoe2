ad728712fd8d17c92389c87a44177d86e86fcae2
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2026-02-27 14:10:05 -0500

    add libdg skeleton

---
+++ source/system/libdg/chanlsys.h
@@ -0,0 +1,10 @@
+#ifndef __ZOE2_CHANLSYS_H__
+#define __ZOE2_CHANLSYS_H__
+
+//
+//
+//
+//
+//
+
+#endif // {{{ END OF FILE }}}
---
+++ source/system/libdg/libdg.h
@@ -0,0 +1,10 @@
+#ifndef __ZOE2_LIBDG_H__
+#define __ZOE2_LIBDG_H__
+
+//
+//
+//
+//
+//
+
+#endif // {{{ END OF FILE }}}
